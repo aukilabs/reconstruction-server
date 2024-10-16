beab3efe8b8e8a16cec21024c1c7b4f68adc0a88
Robin Lindh <robin@matterless.com>
2024-10-16 20:38:16 +0200

    fix local refinement exception on images with few matches
    It threw some exception inside triangulation for some images with few images. They were part of the reconstruction but not in the database cache.

--- utils/triangulation.py
+++ utils/triangulation.py
@@ -21,7 +21,6 @@ def run_triangulation(
 ) -> pycolmap.Reconstruction:
     # Grab logger by name
     logger = logging.getLogger("logger")
-
     mapper_options = pycolmap.IncrementalMapperOptions(options)
 
     database = pycolmap.Database(database_path)
@@ -30,8 +29,15 @@ def run_triangulation(
     ignore_watermarks = True
     image_names = set()
     database_cache = pycolmap.DatabaseCache.create(database, min_num_matches, ignore_watermarks, image_names)
+    #reconstruction = deepcopy(reference_model)
 
-    reconstruction = deepcopy(reference_model)
+    reconstruction = pycolmap.Reconstruction()
+    for img in reference_model.images.values():
+        if database_cache.exists_image(img.image_id):
+            reconstruction.add_image(img)
+    for cam in reference_model.cameras.values():
+        if database_cache.exists_camera(cam.camera_id):
+            reconstruction.add_camera(cam)
 
     clear_points = True
     if clear_points:
