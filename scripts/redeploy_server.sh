31286b3b8c299a5886bee2fdf9c4ac2d89dec0b5
Robin Lindh <robin@matterless.com>
2024-10-23 12:16:50 +0200

    helper scripts to redeploy server binary into running cloud container

---
+++ scripts/redeploy_server.sh
@@ -0,0 +1,13 @@
+HOST="provider.hurricane.akash.pub"
+PORT=32717
+API_KEY="kaffekopp123"
+
+docker build --platform linux/amd64 -f Dockerfile_GO . -t temp-go-build && \
+    ( \
+        docker cp $(docker create --rm temp-go-build):/app/reconstruction ./reconstruction && \
+        scp -P $PORT -i ~/.ssh/id_rsa ./reconstruction root@$HOST:/app/reconstruction-2 && \
+        ssh -p $PORT root@$HOST "chmod +x /app/reconstruction-2 && pkill reconstruction; rm -f /app/reconstruction && mv /app/reconstruction-2 /app/reconstruction" && \
+        ssh -p $PORT root@$HOST "cd /app; nohup ./reconstruction -api-key $API_KEY > log.txt 2>&1 < /dev/null &" \
+    ) && \
+    docker rmi -f temp-go-build && \
+    echo "DONE!"
