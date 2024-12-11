8f0dc0f3cc1cbc85d84c9a2ad50afb2889df4c83
samcts-auki <sam@aukilabs.com>
2024-12-11 03:03:10 +0000

    clean up and comments

--- server/main.go
+++ server/main.go
@@ -29,13 +29,11 @@ func main() {
 	flag.Parse()
 
 	// Configure logging to include file name, line number, and timestamp
-	// log.SetFlags(log.Lshortfile | log.LstdFlags)
 	logs.SetLevel(logs.ParseLevel(*loglevel))
 	logs.Encoder = json.Marshal
 
 	if apiKey == nil || *apiKey == "" {
 		logs.Fatal(errors.New("API key is required"))
-		// log.Fatal("API key is required")
 	}
 	// create a new router
 	r := chi.NewRouter()
@@ -43,11 +41,9 @@ func main() {
 	// Endpoint for triggering refinement jobs (from DMT)
 	r.Post("/jobs", func(w http.ResponseWriter, r *http.Request) {
 		logs.Info("[POST] /jobs endpoint called")
-		// log.Println("[POST] /jobs endpoint called")
 
 		debug, _ := httputil.DumpRequest(r, true)
 		logs.Debug(debug)
-		// log.Printf("%s", debug)
 
 		if apiKey != nil && *apiKey != "" {
 			inApiKey := r.Header.Get("X-API-Key")
@@ -62,33 +58,29 @@ func main() {
 		if jobInProgress {
 			jobMutex.Unlock()
 			logs.Info("Job already in progress, rejecting incoming job request.")
-			// log.Println("Job already in progress, rejecting incoming job request.")
 			http.Error(w, "Reconstruction server is busy processing another job", http.StatusServiceUnavailable)
 			return
 		}
 		jobInProgress = true
 		jobMutex.Unlock()
 
+		// Read request body
 		reqBodyBytes, err := io.ReadAll(r.Body)
 		if err != nil {
 			logs.Error(errors.New("Failed to read request body for job request: " + err.Error()))
-			// log.Print("Failed to read request body for job request")
-			// log.Print(err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			jobMutex.Lock()
 			jobInProgress = false
 			jobMutex.Unlock()
 			return
 		}
-
 		reqBodyString := string(reqBodyBytes)
 		logs.Infof("Request body: %s", reqBodyString)
-		// log.Printf("Request body: %s", reqBodyString)
 
+		// Create job metadata
 		j, err := CreateJobMetadata("jobs", reqBodyString)
 		if err != nil {
 			logs.Error(errors.New("Job creation failed with error: " + err.Error()))
-			// log.Print("Job creation failed with error: ", err.Error())
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			jobMutex.Lock()
 			jobInProgress = false
@@ -96,6 +88,7 @@ func main() {
 			return
 		}
 
+		// Execute Job
 		go func(j job) {
 			defer func() {
 				jobMutex.Lock()
@@ -108,26 +101,22 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 	})
 
+	// Endpoint for fetching current job list
 	r.Get("/jobs", func(w http.ResponseWriter, r *http.Request) {
 		logs.Info("[GET] /jobs endpoint called")
-		// log.Println("[GET] /jobs endpoint called")
+
+		debug, _ := httputil.DumpRequest(r, true)
+		logs.Debug(debug)
 
 		jobList := jobs.List()
 		logs.Info("Number of jobs to list: ", len(jobList), " type: ", reflect.TypeOf(jobList))
-		// log.Println("Number of jobs to list: ", len(jobList), " type: ", reflect.TypeOf(jobList))
-
-		//debug, _ := httputil.DumpRequest(r, true)
-		//log.Printf("%s", debug)
 
 		encoder := json.NewEncoder(w)
 		encoder.SetIndent("", "  ")
-		//dummy := map[string]string{"hello": "world"}
-		//err := encoder.Encode(dummy)
 
 		err := encoder.Encode(jobList)
 		if err != nil {
 			logs.Error(errors.Newf("Error encoding jobs list: %v", err))
-			// log.Printf("Error encoding jobs list: %v", err)
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			return
 		}
