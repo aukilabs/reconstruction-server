184d05ce84e39513cacb5a41b12355da0415387c
tatiesmars <m@thieu.dev>
2025-09-18 09:14:19 +0700

    chore: remove dds package; import posemesh-node-registration

--- server/rust/server-adapters/src/dds/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod crypto;
-pub mod http;
-pub mod persist;
-pub mod register;
-pub mod state;
--- server/rust/server-adapters/src/lib.rs
+++ server/rust/server-adapters/src/lib.rs
@@ -1,4 +1,4 @@
 pub mod config;
-pub mod dds;
+pub use posemesh_node_registration as dds;
 pub mod http;
 pub mod storage;
