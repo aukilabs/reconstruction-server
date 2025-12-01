a2be6e139811eec8322d3f41a5b5a82607536e00
RobinLindh <robin@aukilabs.com>
2025-12-01 10:56:32 +0000

    merge fixes

--- utils/triangulation.py
+++ utils/triangulation.py
@@ -221,53 +221,50 @@ def run_triangulation(
                         bundle_adjuster.problem.set_parameter_upper_bound(camera.params, idx, 0.01)
 
         summary = pyceres.SolverSummary()
+        logger.debug("calling pyceres.solve() ...")
         pyceres.solve(solver_options, bundle_adjuster.problem, summary)
         logger.info("Solved!")
-            summary = pyceres.SolverSummary()
-            logger.debug("calling pyceres.solve() ...")
-            pyceres.solve(solver_options, bundle_adjuster.problem, summary)
-            logger.info("Solved!")
 
-            final_loss_breakdown, final_loss_breakdown_per_image_id = bundle_adjuster.evaluate_loss_breakdown()
+        final_loss_breakdown, final_loss_breakdown_per_image_id = bundle_adjuster.evaluate_loss_breakdown()
 
-            logger.info("------------")
-            logger.info("INITIAL LOSS BREAKDOWN:")
-            for category, loss in initial_loss_breakdown.items():
-                logger.info(f"{category}: {loss}")
-            logger.info("------------")
-            logger.info("FINAL LOSS BREAKDOWN:")
-            for category, loss in final_loss_breakdown.items():
-                logger.info(f"{category}: {loss}")
-            logger.info("------------")
+        logger.info("------------")
+        logger.info("INITIAL LOSS BREAKDOWN:")
+        for category, loss in initial_loss_breakdown.items():
+            logger.info(f"{category}: {loss}")
+        logger.info("------------")
+        logger.info("FINAL LOSS BREAKDOWN:")
+        for category, loss in final_loss_breakdown.items():
+            logger.info(f"{category}: {loss}")
+        logger.info("------------")
 
-            # logger.info("\n".join(summary.FullReport().split(",")))
-            logger.info(f"{summary.FullReport()}")
+        # logger.info("\n".join(summary.FullReport().split(",")))
+        logger.info(f"{summary.FullReport()}")
 
-            num_changed_observations = 0
-            num_changed_observations += mapper.complete_and_merge_tracks(tri_options)
-            num_changed_observations += mapper.filter_points(mapper_options)
+        num_changed_observations = 0
+        num_changed_observations += mapper.complete_and_merge_tracks(tri_options)
+        num_changed_observations += mapper.filter_points(mapper_options)
 
-            changed = num_changed_observations / num_observations
-            logger.info(f'Changed observations: {changed}')
+        changed = num_changed_observations / num_observations
+        logger.info(f'Changed observations: {changed}')
 
-            ba_iterations_remaining -= 1
+        ba_iterations_remaining -= 1
 
-            # Retriangulate underreconstructed image pairs after first BA success
-            if not retriangulated and summary.termination_type == pyceres.TerminationType.CONVERGENCE:
-                logger.info('Retriangulating...')
-                num_retriangulated = mapper.retriangulate(tri_options)
-                logger.info(f'Retriangulated {num_retriangulated} observations')
-                retriangulated = True
+        # Retriangulate underreconstructed image pairs after first BA success
+        if not retriangulated and summary.termination_type == pyceres.TerminationType.CONVERGENCE:
+            logger.info('Retriangulating...')
+            num_retriangulated = mapper.retriangulate(tri_options)
+            logger.info(f'Retriangulated {num_retriangulated} observations')
+            retriangulated = True
 
             # make sure there are at least two more BA iterations after retriangulation
             # (to finish loop closure + filter outliers)
             additional_iterations = max(0, 2 - ba_iterations_remaining)
             num_ba_iterations_total += additional_iterations
             ba_iterations_remaining += additional_iterations
-            
-            if ba_iterations_remaining == 1:
-                ba_options.refine_focal_length = True
-                ba_options.refine_extra_params = True
+        
+        if ba_iterations_remaining == 1:
+            ba_options.refine_focal_length = True
+            ba_options.refine_extra_params = True
 
 
         logger.info('Extracting colors...')
