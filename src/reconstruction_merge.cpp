edf39d1dff7b3f6d38ba22debb0332eab472d958
tatiesmars <m@thieu.dev>
2026-08-19 12:50:35 +0700

    fix(reconstruction): support published COLMAP base

--- src/reconstruction_merge.cpp
+++ src/reconstruction_merge.cpp
@@ -65,11 +65,10 @@ void AppendReconstruction(colmap::Reconstruction& destination,
     }
     destination.AddRig(std::move(new_rig));
 
-    colmap::Frame new_frame = old_frame;
-    new_frame.ResetRigPtr();
+    colmap::Frame new_frame;
     new_frame.SetRigId(new_id);
     new_frame.SetFrameId(new_id);
-    new_frame.ClearDataIds();
+    new_frame.SetRigFromWorld(old_frame.MaybeRigFromWorld());
     new_frame.AddDataId(colmap::data_t(new_sensor, new_id));
     destination.AddFrame(std::move(new_frame));
 
